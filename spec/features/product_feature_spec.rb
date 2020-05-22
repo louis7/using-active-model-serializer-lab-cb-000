224cce4011a475cf94de18287e2545036a100c47
louis baptiste <louisvis@ymail.com>
2020-05-22 21:45:51 +0000

    Automatically backed up by Learn

--- spec/features/product_feature_spec.rb
+++ spec/features/product_feature_spec.rb
@@ -27,19 +27,19 @@ RSpec.describe "Products", type: :feature do
   end
 
   describe "products index" do
-    it 'requires javascript to go next' do
-      p1 = Product.create!(name: "Test Product", inventory: 0, description: "This is a test description with more text than should be there.", price: "2.99")
-      p2 = Product.create!(name: "Test Product 2", inventory: 1, description: "This is a second test description with more text than should be there.", price: "1.99")
+    it 'gets the description and inventory', js: true do
+      product = Product.create!(name: "Test Product", inventory: 0, description: "This is a test description with more text than should be there.")
+      customer = Customer.create(:name => Faker::Name.name)
+      invoice = Invoice.create
+      order = Order.create(customer: customer, invoice: invoice)
 
-      visit product_path(p1)
-      click_link "Next Product"
-    end
-
-    it 'loads next product without page refresh', js: true do
-      p1 = Product.create!(name: "Test Product", inventory: 0, description: "This is a test description with more text than should be there.", price: "2.99")
-      p2 = Product.create!(name: "Test Product 2", inventory: 1, description: "This is a second test description with more text than should be there.", price: "1.99")
-
-      visit product_path(p1)
-      click_link "Next Product"
+      order.products << product
+      visit products_path
+      click_button "More Info"
+      product.inventory = 1
+      product.save
+      visit products_path
+      click_button "More Info"
     end
   end
+end
