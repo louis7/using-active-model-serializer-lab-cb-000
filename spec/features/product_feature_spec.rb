a70052cd2c1ddffc70b0d93701360e16f1ec7847
louis baptiste <louisvis@ymail.com>
2020-05-22 21:45:30 +0000

    Automatically backed up by Learn

--- spec/features/product_feature_spec.rb
+++ spec/features/product_feature_spec.rb
@@ -27,7 +27,19 @@ RSpec.describe "Products", type: :feature do
   end
 
   describe "products index" do
-    
+    it 'requires javascript to go next' do
+      p1 = Product.create!(name: "Test Product", inventory: 0, description: "This is a test description with more text than should be there.", price: "2.99")
+      p2 = Product.create!(name: "Test Product 2", inventory: 1, description: "This is a second test description with more text than should be there.", price: "1.99")
+
+      visit product_path(p1)
+      click_link "Next Product"
+    end
+
+    it 'loads next product without page refresh', js: true do
+      p1 = Product.create!(name: "Test Product", inventory: 0, description: "This is a test description with more text than should be there.", price: "2.99")
+      p2 = Product.create!(name: "Test Product 2", inventory: 1, description: "This is a second test description with more text than should be there.", price: "1.99")
+
+      visit product_path(p1)
+      click_link "Next Product"
     end
   end
-end
